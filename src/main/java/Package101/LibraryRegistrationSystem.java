d816458be614d474f6174f5733b19490ef1fd25c
mariamay2003 <151976047+mariamay2003@users.noreply.github.com>
2023-12-04 00:01:38 +0400

    initial commit

---
+++ src/main/java/Package101/LibraryRegistrationSystem.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Package101;
+
+public class LibraryRegistrationSystem {
+
+    public static void main(String[] args) {
+        
+    }
+}
